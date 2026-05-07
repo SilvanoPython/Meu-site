ec17281b57e1aac1189461f6040760dbe97a5677
SilvanoRosa <silvanorosa@protonmail.com>
2026-05-07 20:34:31 +0000

    Quinto comit

--- myappsite/settings.py
+++ myappsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(-yki60=m^jgi)3hpx687_u6nv+1+&h74ap-+a&_l^95xe8%u)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['SilvanoChitocaPereiraRosa.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['SilvanoChitocaPereiraRosa.pythonanywhere.com','127.0.0.1','www.silvano.ao','silvano.ao']
 
 #silvano
 # Application definition
@@ -117,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
